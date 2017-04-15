447383384a8e30ee2896ce2021fff7ebd314bc5a
Jérémy Li <mail.jeremy.li@gmail.com>
2017-04-15 19:18:32 +1000

    watchify

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,4 @@
 var greet = require('./greeting');
 
 greet('Peeta Mellark');
+greet('Katniss Everdeen');
--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,34 @@ const gutil = require('gulp-util');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const debug = require('gulp-debug');
+const watchify = require('watchify');
+
 // needed to convert the browserify stream and turn it into a gulp stream
 
 const paths = gulp.paths = {
-  scriptEntry: ['./app/scripts/app.js'],
+  entry: './app/scripts/app.js',
   html: ['./app/**/*.html']
 }
 
-gulp.task('scripts', () => {
-  browserify('./app/scripts/app.js')
+function bundleScripts (bundler) {
+  return bundler
     .bundle()
     .on('error', (e) => {
       gutil.log(e);
     })
     .pipe(source('app.js'))
     .pipe(gulp.dest('./www'));
+}
+
+gulp.task('scripts', () => {
+  return bundleScripts(browserify(paths.entry));
+});
+
+gulp.task('watch', () => {
+  const watcher = watchify(browserify(paths.entry, watchify.args));
+  bundleScripts(watcher);
+  watcher.on('update', () => bundleScripts(watcher));
+  watcher.on('log', gutil.log);
 });
 
 gulp.task('templates', () => {
