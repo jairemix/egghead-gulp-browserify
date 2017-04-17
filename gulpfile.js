c53cf2cfcbde0090f50bfe2e4dc95c851309a75d
Jérémy Li <mail.jeremy.li@gmail.com>
2017-04-17 16:13:46 +1000

    fixed build scripts sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -12,29 +12,27 @@ const PATHS = gulp.PATHS = {
   root: __dirname
 };
 
-// task dependencies
-const build = require('./gulp/build');
-const watch = require('./gulp/watch');
-
 /**
-* Compiles HTML and JS in ./app to ./www folder. Then serves the contents of ./www
+* Compiles HTML, JS and SCSS in ./app to ./www folder. Then serves the contents of ./www
 * Both the contents of ./app and ./www are watched
 */
 gulp.task('watch', () => {
+  const watch = require('./gulp/watch');
   let scriptStrm = watch.watchScripts();
   let htmlStrm = watch.watchTemplates();
   let styleStrm = watch.watchStyles();
-  // serve after two streams finish (after first build)
+  // serve after all streams finish (after first build)
   return es.merge(scriptStrm, htmlStrm, styleStrm)
     .on('end', () => watch.serve())
     .on('error', (error) => console.log(chalk.red('Error watching:', error)));
 });
 
 /**
-* Compiles HTML and JS to ./www folder
+* Compiles HTML, JS and SCSS in ./app to ./www folder
 */
 gulp.task('build', () => {
-  let scriptStrm = build.buildScripts(browserify(PATHS.entry));
+  const build = require('./gulp/build');
+  let scriptStrm = build.buildScripts(browserify(PATHS.entry, { debug: true }));
   let htmlStrm = build.buildTemplates();
   let styleStrm = build.buildStyles();
   return es.merge(scriptStrm, htmlStrm, styleStrm);
